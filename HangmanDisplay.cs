10d57c0298819d9860a0adeb7767f44fd487a3cc
OscarSystemDeveloper <oscj87@gmail.com>
2025-01-04 13:54:47 +0100

    Done making the Base

---
+++ HangmanDisplay.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json; // Add Newtonsoft.Json via NuGet for JSON handling
+
+namespace HangmanOscar
+{
+    // Manages the display of the hangman graphics
+    public class HangmanDisplay
+    {
+        public void PrintHangman(int wrong)
+        {
+            string[] stages =
+            {
+                "\n+---+\n    |\n    |\n    |\n   ===",
+                "\n+---+\nO   |\n    |\n    |\n   ===",
+                "\n+---+\nO   |\n|   |\n    |\n   ===",
+                "\n+---+\n O  |\n/|  |\n    |\n   ===",
+                "\n+---+\n O  |\n/|\\ |\n    |\n   ===",
+                "\n+---+\n O  |\n/|\\ |\n/   |\n   ===",
+                "\n+---+\n O  |\n/|\\ |\n/ \\ |\n   ==="
+            };
+
+            Console.WriteLine(stages[wrong]);
+        }
+    }
+}
